66dac9cced4fdb1e907cd8125810bf52b3f907b4
noah <nomaxx117@gmail.com>
2018-12-09 12:22:55 -0600

    Added comments to UML generation.

--- build.rs
+++ build.rs
@@ -1,8 +1,12 @@
 extern crate mml;
 
+fn generate_uml() {
+    let dest: String = concat!("target/doc/", env!("CARGO_PKG_NAME")).to_string();
+    
+    let _ = mml::src2both("src", dest.replace("-", "_").as_str());
+}
+
 
 fn main() {
-    let dest: String = concat!("target/doc/", env!("CARGO_PKG_NAME")).to_string();
-
-    let _ = mml::src2both("src", dest.replace("-", "_").as_str());
+    //generate_uml();
 }
