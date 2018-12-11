b642f1b5d0e9902766c3dc45a0b8609cadbc57ab
noah <nomaxx117@gmail.com>
2018-12-11 13:42:02 -0600

    Fixed formatting

--- src/subsystems/drive_train.rs
+++ src/subsystems/drive_train.rs
@@ -35,28 +35,28 @@ impl Subsystem<DriveTrainError> for DriveTrain {
     fn init(&mut self) -> DriveTrainError {
         unimplemented!()
     }
-    
-    
+
+
     fn run(&mut self) {
         unimplemented!()
     }
-    
-    
+
+
     fn enable(&mut self) {
         self.is_enabled = true;
     }
-    
-    
+
+
     fn disable(&mut self) {
         self.is_enabled = false;
     }
-    
-    
+
+
     fn is_enabled(&self) -> bool {
         self.is_enabled
     }
-    
-    
+
+
     fn if_disabled(&mut self) {
         unimplemented!()
     }
@@ -68,8 +68,8 @@ impl DriveTrain {
         self.right.set_speed(right_speed);
         self.left.set_speed(left_speed);
     }
-    
-    
+
+
     pub fn new(&mut self, logging_channel: Sender<LogData>,
                error_channel: Sender<DriveTrainError>, test_mode: TestMode) -> DriveTrain {
         unimplemented!()
@@ -87,28 +87,27 @@ struct TankSide {
 impl MotorController for TankSide {
     fn set_speed(&mut self, new_speed: f32) -> Result<()> {
         let potentially_inverted_speed = if self.is_inverted() {
-            - new_speed
+            -new_speed
         } else {
             new_speed
         };
-        
+
         self.front.set_speed(potentially_inverted_speed)?;
         self.back.set_speed(potentially_inverted_speed)
     }
-    
+
     fn stop(&mut self) -> Result<()> {
         self.set_speed(0.0)
     }
-    
+
     fn invert(&mut self) {
         self.is_inverted = !self.is_inverted()
     }
-    
+
     fn is_inverted(&self) -> bool {
         self.is_inverted
     }
 }
-
 
 
 impl Device for TankSide {}
