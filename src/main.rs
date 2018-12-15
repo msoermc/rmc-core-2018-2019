e99503657688920c0ffcc77baa1b2ec0091757d8
noah <nomaxx117@gmail.com>
2018-12-15 12:00:49 -0600

    Moved log tester into separate module and out of main function
    Also added code to that test that shows if something has been sent to comms.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,10 @@
-use crate::framework::logging::Logger;
-use std::sync::mpsc::channel;
-use crate::framework::Subsystem;
-use std::thread;
-use std::io::stdin;
-use crate::framework::logging::LogType;
-use crate::framework::logging::LogData;
+use crate::test_programs::test_logging;
 
 pub mod framework;
 pub mod subsystems;
 pub mod devices;
+pub mod test_programs;
 
 fn main() {
-    let (sender, receiver) = channel();
-    let mut logger = Logger::new(sender);
-
-    let log_channel = logger.get_command_sender();
-
-    thread::spawn(move || logger.start());
-
-    loop {
-        let mut buffer = String::new();
-        stdin().read_line(& mut buffer).unwrap();
-        let timestamp = chrono::Utc::now();
-        let severity = LogType::Info;
-
-        let log = LogData::new(severity, timestamp, buffer);
-
-        log_channel.send(log).unwrap();
-    }
+    test_logging::run_test();
 }
---
+++ src/test_programs/test_logging.rs
@@ -0,0 +1,45 @@
+use std::{
+    io::stdin,
+    sync::mpsc::channel,
+    thread,
+};
+
+use crate::{
+    framework::{
+        logging::{
+            LogType,
+            Logger,
+            LogData,
+        },
+        Subsystem,
+    }
+};
+use crate::subsystems::comms::Message;
+
+pub fn run_test() {
+    let (comms_sender, comms_receiver) = channel();
+    let mut logger = Logger::new(comms_sender);
+
+    let log_channel = logger.get_command_sender();
+
+    thread::spawn(move || logger.start());
+
+    thread::spawn(move || {
+        loop {
+            let Message::Log(log) = comms_receiver.recv().unwrap();
+            println!("[COMMS]:\t{}", log.to_string());
+        }
+    });
+
+    loop {
+        let mut buffer = String::new();
+        println!("Please enter a single-line log message.");
+        stdin().read_line(&mut buffer).unwrap();
+        let timestamp = chrono::Utc::now();
+        let severity = LogType::Info;
+
+        let log = LogData::new(severity, timestamp, buffer);
+
+        log_channel.send(log).unwrap();
+    }
+}
