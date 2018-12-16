bdf51b5fdfdb384a36fea3dddde028101b73d10e
noah <nomaxx117@gmail.com>
2018-12-16 13:26:52 -0600

    Cleaned up external comms to redo everything

--- src/comms/external_comms.rs
+++ src/comms/external_comms.rs
@@ -4,15 +4,16 @@ use std::{
         mpsc::{
             Sender,
             Receiver,
-            TryRecvError,
         }
     },
     thread::spawn,
 };
-use std::io::BufReader;
 
-use tokio::net::TcpListener;
-use tokio::net::TcpStream;
+use tokio::{
+    net::TcpListener,
+    net::TcpStream,
+    prelude::stream::Stream,
+};
 
 use crate::{
     framework::{
@@ -52,10 +53,12 @@ pub struct ExternalComms {
     sending_channel: Receiver<SendableMessage>,
     logging_channel: Sender<LogData>,
     listener: TcpListener,
-    clients: Vec<BufReader<TcpStream>>,
+    clients: Vec<TcpStream>
 }
 
 impl ExternalComms {
+    /// Instantiates the comms.
+    /// This constructor will bind the listener.
     pub fn new(logging_channel: Sender<LogData>, sending_channel: Receiver<SendableMessage>) -> ExternalComms {
         let address = SocketAddr::new(
             ADDRESS.parse().expect("Could not parse address"),
@@ -77,19 +80,18 @@ impl ExternalComms {
                 } else {
                     panic!("Could not bind listener for external comms and could not log either!");
                 }
-            },
+            }
         };
-
-        let clients = Vec::new();
 
         ExternalComms {
             sending_channel,
             logging_channel,
             listener,
-            clients,
+            clients: Vec::new()
         }
     }
 
+    /// Starts the comms in a <b>new</b> thread.
     pub fn start(mut self) {
         spawn(move || {
             loop {
@@ -98,95 +100,21 @@ impl ExternalComms {
         });
     }
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    // Running loop
-    ///////////////////////////////////////////////////////////////////////////////////////////////
     fn run(&mut self) {
-        self.check_for_new_connections();
+        self.check_connections();
         self.receive_messages();
         self.send_messages();
     }
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    // Checking for new connections
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    fn check_for_new_connections(&mut self) {
+    fn check_connections(&mut self) {
         unimplemented!()
     }
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    // Sending messages
-    ///////////////////////////////////////////////////////////////////////////////////////////////
     fn send_messages(&mut self) {
-        match self.sending_channel.try_recv() {
-            Ok(message) => self.handle_new_sendable_message(message),
-            Err(e) => {
-                if TryRecvError::Disconnected == e {
-                    self.handle_sending_channel_hangup();
-                }
-            }
-        }
-    }
-
-    fn handle_sending_channel_hangup(&mut self) {
-        let severity = LogType::Fatal;
-        let description = "Sending channel for external comms hung up".to_string();
-        let timestamp = get_timestamp();
-        let log = LogData::new(severity, timestamp, description);
-        self.logging_channel.send(log).unwrap(); // fail fast if the logger dies
-    }
-
-    fn handle_new_sendable_message(&mut self, message: SendableMessage) {
         unimplemented!()
     }
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    // Receiving messages
-    ///////////////////////////////////////////////////////////////////////////////////////////////
     fn receive_messages(&mut self) {
         unimplemented!()
     }
-
-    fn handle_new_receivable_message(&mut self, message: ReceivableMessage) {
-        match message {
-            ReceivableMessage::Kill => self.handle_kill(),
-            ReceivableMessage::Revive => self.handle_revive(),
-            ReceivableMessage::Enable(subsystem) => self.handle_enable(subsystem),
-            ReceivableMessage::Disable(subsystem) => self.handle_disable(subsystem),
-            ReceivableMessage::Drive(left, right) => self.handle_drive(left, right),
-            ReceivableMessage::Brake => self.handle_brake(),
-        }
-    }
-
-    fn handle_kill(&mut self) {
-        unimplemented!()
-    }
-
-    fn handle_revive(&mut self) {
-        unimplemented!()
-    }
-
-    fn handle_enable(&mut self, subsystem: ProtocolSubsystem) {
-        unimplemented!()
-    }
-
-    fn handle_disable(&mut self, subsystem: ProtocolSubsystem) {
-        unimplemented!()
-    }
-
-    fn handle_brake(&mut self) {
-        unimplemented!()
-    }
-
-    fn handle_drive(&mut self, left_speed: f32, right_speed: f32) {
-        unimplemented!()
-    }
-
-    fn handle_log(&mut self) {
-        unimplemented!()
-    }
-}
-
-fn parse_receivable_message(message: String) -> Result<ReceivableMessage, ParsingError> {
-    unimplemented!()
 }
