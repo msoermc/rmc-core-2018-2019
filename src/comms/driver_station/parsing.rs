0bb217baf3aff34e0e0115cbfc6f55b8039c449b
noah <nomaxx117@gmail.com>
2018-12-18 13:43:48 -0600

    Added tests for drive parsing and changed iterators to vectors

--- src/comms/driver_station/parsing.rs
+++ src/comms/driver_station/parsing.rs
@@ -3,12 +3,12 @@ use std::str::SplitWhitespace;
 use crate::comms::get_wrong_arg_count_log;
 use crate::framework::logging::LogData;
 
-#[derive(Copy, Clone, Eq, PartialEq)]
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum ProtocolSubsystem {
     DriveTrain,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 pub enum ReceivableMessage {
     Kill,
     Revive,
@@ -20,9 +20,9 @@ pub enum ReceivableMessage {
 
 
 pub fn parse_message(message: &str) -> Result<ReceivableMessage, LogData> {
-    let mut elements = message.split_whitespace();
-    let command = match elements.next() {
-        Some(com) => com,
+    let mut elements: Vec<&str> = message.split_whitespace().collect();
+    let command = match elements.first() {
+        Some(com) => *com,
         None => {
             unimplemented!()
         }
@@ -46,15 +46,15 @@ pub fn parse_message(message: &str) -> Result<ReceivableMessage, LogData> {
 }
 
 
-fn parse_drive_command(original_message: &str, mut args: SplitWhitespace) -> Result<ReceivableMessage, LogData> {
-    if args.by_ref().count() != 2 as usize {
-        let log = get_wrong_arg_count_log(original_message, 2, args.count() as u64);
+fn parse_drive_command(original_message: &str, mut args: Vec<&str>) -> Result<ReceivableMessage, LogData> {
+    if args.len() != 3 as usize {
+        let log = get_wrong_arg_count_log(original_message, 2, args.len() as u64);
         Err(log)
     } else {
         // It should not be possible here for us to have too few arguments since we already
         // checked our that, so it should be safe to unwrap
-        let left_speed_string = args.next().unwrap();
-        let right_speed_string = args.next().unwrap();
+        let left_speed_string = args[1];
+        let right_speed_string = args[2];
 
         let left_speed: f32 = match left_speed_string.parse() {
             Ok(speed) => speed,
@@ -76,22 +76,22 @@ fn parse_drive_command(original_message: &str, mut args: SplitWhitespace) -> Res
     }
 }
 
-fn parse_enable_command(original_message: &str, mut args: SplitWhitespace) -> Result<ReceivableMessage, LogData> {
-    if args.by_ref().count() != 1 {
-        let log = get_wrong_arg_count_log(original_message, 1, args.count() as u64);
+fn parse_enable_command(original_message: &str, mut args: Vec<&str>) -> Result<ReceivableMessage, LogData> {
+    if args.len() != 2 {
+        let log = get_wrong_arg_count_log(original_message, 1, args.len() as u64);
         Err(log)
     } else {
-        let parsed_subsystem = parse_subsystem(args.next().unwrap())?;
+        let parsed_subsystem = parse_subsystem(args[1])?;
         Ok(ReceivableMessage::Enable(parsed_subsystem))
     }
 }
 
-fn parse_disable_command(original_message: &str, mut args: SplitWhitespace) -> Result<ReceivableMessage, LogData> {
-    if args.by_ref().count() != 1 {
-        let log = get_wrong_arg_count_log(original_message, 1, args.count() as u64);
+fn parse_disable_command(original_message: &str, mut args: Vec<&str>) -> Result<ReceivableMessage, LogData> {
+    if args.len() != 2 {
+        let log = get_wrong_arg_count_log(original_message, 1, args.len() as u64);
         Err(log)
     } else {
-        let parsed_subsystem = parse_subsystem(args.next().unwrap())?;
+        let parsed_subsystem = parse_subsystem(args[1])?;
         Ok(ReceivableMessage::Disable(parsed_subsystem))
     }
 }
@@ -103,29 +103,49 @@ fn parse_subsystem(field: &str) -> Result<ProtocolSubsystem, LogData> {
     }
 }
 
-fn parse_revive_command(original_message: &str, mut args: SplitWhitespace) -> Result<ReceivableMessage, LogData> {
-    if args.by_ref().count() != 0 {
-        let log = get_wrong_arg_count_log(original_message, 0, args.count() as u64);
+fn parse_revive_command(original_message: &str, mut args: Vec<&str>) -> Result<ReceivableMessage, LogData> {
+    if args.len() != 1 {
+        let log = get_wrong_arg_count_log(original_message, 0, args.len() as u64);
         Err(log)
     } else {
         Ok(ReceivableMessage::Revive)
     }
 }
 
-fn parse_kill_command(original_message: &str, mut args: SplitWhitespace) -> Result<ReceivableMessage, LogData> {
-    if args.by_ref().count() != 0 {
-        let log = get_wrong_arg_count_log(original_message, 0, args.count() as u64);
+fn parse_kill_command(original_message: &str, mut args: Vec<&str>) -> Result<ReceivableMessage, LogData> {
+    if args.len() != 1 {
+        let log = get_wrong_arg_count_log(original_message, 0, args.len() as u64);
         Err(log)
     } else {
         Ok(ReceivableMessage::Kill)
     }
 }
 
-fn parse_brake_command(original_message: &str, mut args: SplitWhitespace) -> Result<ReceivableMessage, LogData> {
-    if args.by_ref().count() != 0 {
-        let log = get_wrong_arg_count_log(original_message, 0, args.count() as u64);
+fn parse_brake_command(original_message: &str, mut args: Vec<&str>) -> Result<ReceivableMessage, LogData> {
+    if args.len() != 1 {
+        let log = get_wrong_arg_count_log(original_message, 0, args.len() as u64);
         Err(log)
     } else {
         Ok(ReceivableMessage::Brake)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_drive_parsing() {
+        let valid_f_f = parse_message("drive 2.0 1.0").unwrap();
+        let valid_i_i = parse_message("drive 2 1").unwrap();
+        let valid_i_f = parse_message("drive 2 1.0").unwrap();
+        let valid_f_i = parse_message("drive 2.0 1").unwrap();
+
+        let expected = ReceivableMessage::Drive(2.0, 1.0);
+
+        assert_eq!(valid_f_f, expected);
+        assert_eq!(valid_i_i, expected);
+        assert_eq!(valid_i_f, expected);
+        assert_eq!(valid_f_i, expected);
     }
 }
