46a5ddefcb2371023ac976e4b1362dd349a91c61
noah <nomaxx117@gmail.com>
2018-12-21 08:43:03 -0600

    Added a test for stopping the DriveTrain and added comments to Runnable

--- src/drive_train/tests.rs
+++ src/drive_train/tests.rs
@@ -219,3 +219,28 @@ fn test_interactions() {
     let right_result = right_test_receiver.recv_timeout(Duration::from_millis(TIMEOUT)).unwrap();
     assert_eq!(TestAction::SetSpeed(1.0), right_result);
 }
+
+#[test]
+fn test_stop() {
+    let (left_test_sender, left_test_receiver) = channel();
+    let (right_test_sender, right_test_receiver) = channel();
+    let (log_sender, log_receiver) = channel();
+    let (command_sender, command_receiver) = channel();
+
+    let left_motor = TestMotor::new(left_test_sender);
+    let right_motor = TestMotor::new(right_test_sender);
+
+    let mut drive_train = DriveTrain::new(command_receiver, log_sender, Box::new(left_motor), Box::new(right_motor));
+
+    spawn(move || {
+        drive_train.start();
+    });
+
+    command_sender.send(DriveTrainCommand::Stop).unwrap();
+
+    let left_result = left_test_receiver.recv_timeout(Duration::from_millis(TIMEOUT)).unwrap();
+    assert_eq!(TestAction::Stop, left_result);
+
+    let right_result = right_test_receiver.recv_timeout(Duration::from_millis(TIMEOUT)).unwrap();
+    assert_eq!(TestAction::Stop, right_result);
+}
--- src/framework/mod.rs
+++ src/framework/mod.rs
@@ -1,19 +1,18 @@
 pub mod logging;
 
-/// The subsystem trait represents different subsystems on the robot such as bucket ladders,
-/// drivetrains, and dumping mechanisms.
-///
-/// Subsystems are run in their own processes concurrently by the robot framework.
+/// The runnable trait represents a process which should initialize itself and run repeatedly.
 pub trait Runnable {
-    /// Initializes the subsystem, returning a result object indicating whether the action was
+    /// Initializes the Runnable, returning a result object indicating whether the action was
     /// successful.
     fn init(&mut self);
 
-    /// Runs a single loop of the subsystem. This function will be called repeatedly by the
+    /// Runs a single loop of the Runnable. This function will be called repeatedly by the
     /// framework.
+    ///
+    /// This function should return false when this Runnable is stopping.
     fn run(&mut self) -> bool;
 
-    /// Starts the subsystem in the current thread. The subsystem will take over the current thread
+    /// Starts the Runnable in the current thread. The Runnable will take over the current thread
     /// when this method is invoked.
     fn start(&mut self) {
         self.init();
