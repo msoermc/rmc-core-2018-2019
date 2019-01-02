5156edaf1a087e73c991fed13806eac6a8359b26
noah <nomaxx117@gmail.com>
2019-01-02 12:01:25 -0600

    Renamed interfaces

--- src/drive_train/interface.rs
+++ src/drive_train/interface.rs
@@ -1,15 +1,33 @@
-use crate::drive_train::DriveTrainCommand;
 use std::sync::mpsc::Sender;
+
+use crate::drive_train::DriveTrainCommand;
 use crate::framework::interfaces::EnablingInterface;
-use crate::logging::log_data::LogData;
 use crate::framework::interfaces::RobotInterface;
+use crate::framework::interfaces::TankDriveInterface;
+use crate::logging::log_data::LogData;
 
 #[derive(Clone, Debug)]
-pub struct DriveTrainInterface {
+pub struct ConcreteTankDriveInterface {
     channel: Sender<DriveTrainCommand>,
 }
 
-impl EnablingInterface for DriveTrainInterface {
+impl TankDriveInterface for ConcreteTankDriveInterface {
+    fn drive(&self, left_speed: f32, right_speed: f32) -> Result<(), LogData> {
+        match self.channel.send(DriveTrainCommand::Drive(left_speed, right_speed)) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(LogData::fatal("Drive Train drive command hung up!")),
+        }
+    }
+
+    fn brake(&self) -> Result<(), LogData> {
+        match self.channel.send(DriveTrainCommand::Stop) {
+            Ok(_) => Ok(()),
+            Err(_) => Err(LogData::fatal("Drive Train stop command hung up!")),
+        }
+    }
+}
+
+impl EnablingInterface for ConcreteTankDriveInterface {
     fn enable(&self) -> Result<(), LogData> {
         match self.channel.send(DriveTrainCommand::Enable) {
             Ok(_) => Ok(()),
@@ -25,13 +43,11 @@ impl EnablingInterface for DriveTrainInterface {
     }
 }
 
-impl RobotInterface for DriveTrainInterface {
+impl RobotInterface for ConcreteTankDriveInterface {}
 
-}
-
-impl DriveTrainInterface {
+impl ConcreteTankDriveInterface {
     pub fn new(channel: Sender<DriveTrainCommand>) -> Self {
-        DriveTrainInterface {
+        ConcreteTankDriveInterface {
             channel,
         }
     }
--- src/framework/interfaces.rs
+++ src/framework/interfaces.rs
@@ -9,7 +9,7 @@ pub trait EnablingInterface: RobotInterface {
     fn disable(&self) -> Result<(), LogData>;
 }
 
-pub trait DriveTrainInterface: EnablingInterface + RobotInterface {
+pub trait TankDriveInterface: EnablingInterface + RobotInterface {
     fn drive(&self, left_speed: f32, right_speed: f32) -> Result<(), LogData>;
     fn brake(&self) -> Result<(), LogData>;
 }
