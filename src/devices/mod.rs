7708cf7a5297fe4e8f19b59b8c6c7fc52394aeb6
noah <nomaxx117@gmail.com>
2019-01-05 11:59:59 -0600

    Added code to enable PWM

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -1,2 +1,31 @@
+use std::process::Command;
+
+use crate::logging::log_data::LogData;
+
 pub mod motor_controllers;
 pub mod sensors;
+
+pub fn enable_pwm() {
+    // Enable PWM Drivers
+    Command::new("echo ").
+        arg("am33xx_pwm")
+        .arg(">")
+        .arg("/sys/devices/platform/bone_capemgr/slots")
+        .output()
+        .expect("Failed to enable am33xx_pwm driver!");
+
+    Command::new("echo ").
+        arg("cape-universal")
+        .arg(">")
+        .arg("/sys/devices/platform/bone_capemgr/slots")
+        .output()
+        .expect("Failed to enable universal cape driver!");
+
+    // Export chip
+    Command::new("echo ").
+        arg("1")
+        .arg(">")
+        .arg("/sys/class/pwm/pwmchip6/export")
+        .output()
+        .expect("Failed to export chip!");
+}
