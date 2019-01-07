a6565261d26ef6acb799ec9b28626faadbdfa72c
noah <nomaxx117@gmail.com>
2019-01-07 09:11:45 -0600

    Changed variable names in deploy.sh for better legibility

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Set our toolchain
-TOOLCHAIN="armv7-unknown-linux-musleabihf" # armv7 with hardware floating-point
-#TOOLCHAIN="arm-unknown-linux-musleabihf" # arm with hardware floating-point
-#TOOLCHAIN="arm-unknown-linux-musleabi" # arm with software floating-point
+CROSS_COMPILER_TOOLCHAIN="armv7-unknown-linux-musleabihf" # armv7 with hardware floating-point
+#CROSS_COMPILER_TOOLCHAIN="arm-unknown-linux-musleabihf" # arm with hardware floating-point
+#CROSS_COMPILER_TOOLCHAIN="arm-unknown-linux-musleabi" # arm with software floating-point
 
-USER="noah"
+REMOTE_USER="noah"
 
 # Cross compile the code
-cross build --release --target=${TOOLCHAIN}
+cross build --release --target=${CROSS_COMPILER_TOOLCHAIN}
 
 # Copy the code to the home folder of the msoermc user on the other device.
 # scp {src} {dest}
-sudo scp ./target/${TOOLCHAIN}/release/rmc-beaglebone-core-2018-2019 ${USER}@192.168.7.2:/home/${USER}/
+sudo scp ./target/${CROSS_COMPILER_TOOLCHAIN}/release/rmc-beaglebone-core-2018-2019 ${REMOTE_USER}@192.168.7.2:/home/${REMOTE_USER}/
