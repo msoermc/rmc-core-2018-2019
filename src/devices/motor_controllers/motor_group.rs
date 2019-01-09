53d7de93096461ca17cf7d0a7e47cd61e6be3e48
noah <nomaxx117@gmail.com>
2019-01-09 14:19:30 -0600

    Implemented motor group fully

--- src/devices/motor_controllers/motor_group.rs
+++ src/devices/motor_controllers/motor_group.rs
@@ -4,6 +4,7 @@ use crate::devices::motor_controllers::MotorFailure;
 pub struct MotorGroup {
     is_inverted: bool,
     motors: Vec<Box<MotorController>>,
+    old_speed: f32,
 }
 
 impl MotorGroup {
@@ -11,31 +12,37 @@ impl MotorGroup {
         MotorGroup {
             is_inverted: false,
             motors,
+            old_speed: 0.0,
         }
     }
 
     pub fn set_speed(&mut self, new_speed: f32) -> Result<(), Vec<MotorFailure>> {
-        self.run_operation(|mut motor| motor.set_speed(new_speed))
+        self.run_operation(|motor| motor.set_speed(new_speed))
     }
 
     pub fn stop(&mut self) -> Result<(), Vec<MotorFailure>> {
-        self.set_speed(0.0)
+        self.run_operation(|motor| motor.stop())
     }
 
     pub fn invert(&mut self) -> Result<(), Vec<MotorFailure>> {
-        unimplemented!()
+        self.is_inverted = !self.is_inverted;
+        self.run_operation(|motor| motor.invert())
     }
 
-    pub fn is_inverted(&self) -> Result<bool, Vec<MotorFailure>> {
-        unimplemented!()
+    pub fn is_inverted(&self) -> bool {
+        self.is_inverted
     }
 
     pub fn maintain_last(&mut self) -> Result<(), Vec<MotorFailure>> {
-        unimplemented!()
+        self.set_speed(self.old_speed)
     }
 
     fn run_operation<T: Fn(&mut Box<MotorController>) -> Result<(), MotorFailure>>(&mut self, operation: T) -> Result<(), Vec<MotorFailure>> {
-        let results: Vec<MotorFailure> = self.motors.iter_mut().map(operation).filter_map(|res| res.err()).collect();
+        let results: Vec<MotorFailure> =
+            self.motors.iter_mut()
+                .map(operation)
+                .filter_map(|res| res.err())
+                .collect();
 
         if results.is_empty() {
             Ok(())
