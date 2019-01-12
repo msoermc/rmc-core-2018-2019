d8db58e3c0e82015efe2da0e1e7aff54ea8ba942
noah <nomaxx117@gmail.com>
2019-01-12 11:18:37 -0600

    Added initial implementation of Runnable methods to the Controller

--- src/robot_control/controller.rs
+++ src/robot_control/controller.rs
@@ -1,24 +1,59 @@
+use std::sync::Arc;
 use std::sync::mpsc::Receiver;
+use std::sync::RwLock;
 
 use crate::comms::CommsView;
+use crate::framework::Runnable;
 use crate::logging::log_sender::LogSender;
 use crate::robot_control::drive_train::DriveTrain;
 use crate::robot_control::RobotControllerCommand;
-use crate::framework::Runnable;
+use crate::robot_control::RobotLifeStatus;
 
 pub struct RobotController {
     log_view: LogSender,
     driver_station_view: CommsView,
     command_receiver: Receiver<RobotControllerCommand>,
     drive_train: DriveTrain,
+    life_status: Arc<RwLock<RobotLifeStatus>>,
 }
 
 impl Runnable for RobotController {
     fn init(&mut self) {
-        unimplemented!()
+        // Do nothing
     }
 
     fn run(&mut self) {
-        unimplemented!()
+        if let Ok(message) = self.command_receiver.try_recv() {
+            match message {
+                RobotControllerCommand::Drive(drive_command) => {
+                    self.drive_train.drive(drive_command.left_speed, drive_command.right_speed);
+                }
+                RobotControllerCommand::Brake => {
+                    self.drive_train.stop();
+                }
+                RobotControllerCommand::Enable => {
+                    self.drive_train.enable();
+                }
+                RobotControllerCommand::Disable => {
+                    self.drive_train.disable();
+                }
+            }
+        }
+
+        self.drive_train.run_cycle();
+    }
+}
+
+impl RobotController {
+    pub fn new(log_view: LogSender, driver_station_view: CommsView,
+           command_receiver: Receiver<RobotControllerCommand>,
+           drive_train: DriveTrain, life_status: Arc<RwLock<RobotLifeStatus>>) -> Self {
+            Self {
+                log_view,
+                driver_station_view,
+                command_receiver,
+                drive_train,
+                life_status
+            }
     }
 }
