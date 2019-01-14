969f4eddfe099656572ab73df0b8f847ba8bc17d
noah <nomaxx117@gmail.com>
2019-01-13 23:55:24 -0600

    Changed crate name

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,5 @@ cross build --release --target=${CROSS_COMPILER_TOOLCHAIN}
 
 # Copy the code to the home folder of the msoermc user on the other device.
 # scp {src} {dest}
-sudo sshpass -p ${REMOTE_PASSWORD} scp ./target/${CROSS_COMPILER_TOOLCHAIN}/release/rmc-beaglebone-core-2018-2019 ${REMOTE_USER}@192.168.7.2:/home/${REMOTE_USER}/
+sudo sshpass -p ${REMOTE_PASSWORD} scp ./target/${CROSS_COMPILER_TOOLCHAIN}/release/rmc-core ${REMOTE_USER}@192.168.7.2:/home/${REMOTE_USER}/
 sudo sshpass -p ${REMOTE_PASSWORD} scp ./enable-pwm.sh ${REMOTE_USER}@192.168.7.2:/home/${REMOTE_USER}/
