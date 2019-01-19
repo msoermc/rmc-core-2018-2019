cd25231dd6bbdc8421e5151ff27345037c231ddd
irondeau <isaiahrondeau@gmail.com>
2019-01-19 17:07:36 -0600

    Add standard html, css, and javascript source files

---
+++ static/style.js
@@ -0,0 +1,23 @@
+//function post() {
+//    let command = $("#command-input").val();
+//    let url = $(location).attr('href');
+//    let send = url + command;
+//
+//    console.log("Post " + send);
+//
+//    $.post(send);
+//}
+$(document).ready(function() {
+    // DOCUMENT READY
+});
+
+function post() {
+    var hostname = $(location).attr('host');
+    fetch(hostname) // Call fetch function, passing the URL of the server as a parameter
+    .then(function (value) {
+        // Handle data received from server
+    })
+    .catch(function (reason) {
+        // Catch errors that are thrown
+    });
+}
