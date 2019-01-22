cd265dadfc7279b81642e4264bf2f1541e3de244
noah <nomaxx117@gmail.com>
2019-01-21 20:14:21 -0600

    Got sample HTML where we are proving that we can send commands.

--- static/style.js
+++ static/style.js
@@ -3,8 +3,38 @@ $(document).ready(function () {
 });
 
 function post_drive() {
-    let url = "/drive/1.0/1.0";
-    console.log(url);
+    let left = $("#left-drive").val();
+    let right = $("#right-drive").val();
+    let url = "/drive/" + left + "/" + right;
+    post_command(url);
+}
+
+function post_kill() {
+    let url = "/kill";
+    post_command(url);
+}
+
+function post_revive() {
+    let url = "/revive";
+    post_command(url);
+}
+
+function post_enable_drive_train() {
+    let url = "/enable/drive_train";
+    post_command(url);
+}
+
+function post_disable_drive_train() {
+    let url = "/disable/drive_train";
+    post_command(url);
+}
+
+function post_brake() {
+    let url = "/brake";
+    post_command(url);
+}
+
+function post_command(url) {
     fetch(url, {method: "POST"})
         .then(function (value) {
             // I'll be damned if I know what to do here.
