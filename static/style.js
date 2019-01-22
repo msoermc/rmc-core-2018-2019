3623be8ee7b473037d500457eeb989e0bb7afa9c
noah <nomaxx117@gmail.com>
2019-01-21 19:44:49 -0600

    Added relevant comments to frontend

--- static/style.js
+++ static/style.js
@@ -1,24 +1,15 @@
-//function post() {
-//    let command = $("#command-input").val();
-//    let url = $(location).attr('href');
-//    let send = url + command;
-//
-//    console.log("Post " + send);
-//
-//    $.post(send);
-//}
-$(document).ready(function() {
+$(document).ready(function () {
     // DOCUMENT READY
 });
 
 function post_drive() {
     let url = "/drive/1.0/1.0";
     console.log(url);
-    fetch(url, {method: "POST"}) // Call fetch function, passing the URL of the server as a parameter
-    .then(function (value) {
-        alert("Succeeded: " + value);
-    })
-    .catch(function (reason) {
-        alert("Failed: " + value);
-    });
+    fetch(url, {method: "POST"})
+        .then(function (value) {
+            // I'll be damned if I know what to do here.
+        })
+        .catch(function (reason) {
+            // I'll be damned if I know what to do here.
+        });
 }
