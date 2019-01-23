501e96ae47ec6f06560b819eb0e2f44a49eee423
noah <nomaxx117@gmail.com>
2019-01-23 15:42:49 -0600

    Changing resolution paths for robot resources in client

--- static/requests.js
+++ static/requests.js
@@ -1,32 +1,32 @@
 function post_drive() {
     let left = $("#left-drive").val();
     let right = $("#right-drive").val();
-    let url = "/drive/" + left + "/" + right;
+    let url = "/robot/drive_train/drive/" + left + "/" + right;
     post_command(url);
 }
 
 function post_kill() {
-    let url = "/kill";
+    let url = "/robot/kill";
     post_command(url);
 }
 
 function post_revive() {
-    let url = "/revive";
+    let url = "/robot/revive";
     post_command(url);
 }
 
 function post_enable_drive_train() {
-    let url = "/enable/drive_train";
+    let url = "/robot/drive_train/enable";
     post_command(url);
 }
 
 function post_disable_drive_train() {
-    let url = "/disable/drive_train";
+    let url = "/robot/drive_train/disable";
     post_command(url);
 }
 
 function post_brake() {
-    let url = "/brake";
+    let url = "/robot/drive_train/brake";
     post_command(url);
 }
 
