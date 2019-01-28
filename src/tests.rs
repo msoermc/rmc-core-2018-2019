1bf0d66b44241e9ad3d4d73ea959f75f9e952c24
noah <nomaxx117@gmail.com>
2019-01-28 16:11:56 -0600

    Added integration tests for the digger

--- src/tests.rs
+++ src/tests.rs
@@ -56,6 +56,36 @@ fn test_drive() {
 }
 
 #[test]
+fn test_dig() {
+    let (digger, rails) = create_groups();
+    let mut builder = RobotBuilder::new();
+
+    builder.use_custom_intake(digger.motor_group, rails.motor_group);
+
+    let client = builder.build().launch_tester();
+
+    let status = client.post("/robot/intake/digger/dig").dispatch().status();
+    sleep(Duration::from_millis(TIMEOUT));
+    assert_eq!(Status::Ok, status);
+    assert_eq!(DIGGING_RATE, *digger.speed.read().unwrap());
+}
+
+#[test]
+fn test_stop_digging() {
+    let (digger, rails) = create_groups();
+    let mut builder = RobotBuilder::new();
+
+    builder.use_custom_intake(digger.motor_group, rails.motor_group);
+
+    let client = builder.build().launch_tester();
+
+    let status = client.post("/robot/intake/digger/stop").dispatch().status();
+    sleep(Duration::from_millis(TIMEOUT));
+    assert_eq!(Status::Ok, status);
+    assert_eq!(0.0, *digger.speed.read().unwrap());
+}
+
+#[test]
 fn test_dump() {
     let (_, dumper) = create_groups();
     let mut builder = RobotBuilder::new();
