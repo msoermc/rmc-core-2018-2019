d09f1632cb3c059a0767b9bbaa00db9a5cc2708c
noah <nomaxx117@gmail.com>
2019-02-04 22:46:14 -0600

    Added test pwm

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -1,6 +1,8 @@
 use std::io;
 use std::process::Command;
-
+use std::sync::Arc;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
 pub mod motor_controllers;
 pub mod sensors;
 pub mod sysfs_pin_wrappers;
@@ -25,4 +27,42 @@ pub trait DigitalOutput: Send {
 
 pub trait AnalogOutput: Send {
     fn set_value(&mut self, val: f32) -> Result<(), String>;
+}
+
+pub struct TestPin {
+    state: Arc<AtomicBool>,
+}
+
+impl DigitalOutput for TestPin {
+    fn set_value(&mut self, val: bool) -> Result<(), String> {
+        self.state.swap(val, Ordering::SeqCst);
+        Ok(())
+    }
+}
+
+impl TestPin {
+    pub fn new(state: Arc<AtomicBool>) -> Self {
+        Self {
+            state
+        }
+    }
+}
+
+pub struct TestPwm {
+    state: Arc<atomic::Atomic<f32>>,
+}
+
+impl AnalogOutput for TestPwm {
+    fn set_value(&mut self, val: f32) -> Result<(), String> {
+        self.state.swap(val, atomic::Ordering::SeqCst);
+        Ok(())
+    }
+}
+
+impl TestPwm {
+    pub fn new(state: Arc<atomic::Atomic<f32>>) -> Self {
+        Self {
+            state
+        }
+    }
 }
