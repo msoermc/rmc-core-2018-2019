ed452902a6d61052c5f2979d6518b52f82bb4c8b
noah <nomaxx117@gmail.com>
2019-02-05 19:28:14 -0600

    Cleaned up drive code

--- src/mechatronics/drive_train/mod.rs
+++ src/mechatronics/drive_train/mod.rs
@@ -1,8 +1,8 @@
-use crate::devices::motor_controllers::motor_group::MotorGroup;
-use crate::devices::motor_controllers::MotorState;
-use crate::status::life::GlobalLifeState;
-use crate::mechatronics::drive_train::state::GlobalDriveTrainState;
 use std::sync::Arc;
+
+use crate::devices::motor_controllers::motor_group::MotorGroup;
+use crate::mechatronics::drive_train::state::GlobalDriveTrainState;
+use crate::status::life::GlobalLifeState;
 
 #[cfg(test)]
 mod tests;
@@ -30,7 +30,8 @@ impl DriveTrain {
     /// Runs a cycle of the drive train, instructing all motors to do what they did last time.
     pub fn run_cycle(&mut self) {
         if self.state.get_enabled() && self.robot_status.is_alive() {
-            self.maintain_last();
+            self.left.maintain_last();
+            self.right.maintain_last();
         } else {
             self.brake();
         }
@@ -42,8 +43,7 @@ impl DriveTrain {
             self.left.set_speed(left_speed);
             self.right.set_speed(right_speed);
         } else {
-            self.left.stop();
-            self.right.stop();
+            self.brake();
         }
     }
 
@@ -62,16 +62,5 @@ impl DriveTrain {
     pub fn disable(&mut self) {
         self.state.set_enabled(false);
         self.brake();
-    }
-
-    pub fn get_motor_states(&self) -> Vec<MotorState> {
-        let mut states = self.left.get_states();
-        states.append(&mut self.right.get_states());
-        states
-    }
-
-    fn maintain_last(&mut self) {
-        self.left.maintain_last();
-        self.right.maintain_last();
     }
 }
