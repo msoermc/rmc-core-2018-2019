b9f45041c7f05b37461e8e8e90147c6643681f63
noah <nomaxx117@gmail.com>
2019-02-06 22:57:08 -0600

    Added a test for motor groups

--- src/devices/motor_controllers/motor_group.rs
+++ src/devices/motor_controllers/motor_group.rs
@@ -37,3 +37,34 @@ impl MotorGroup {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use crate::devices::motor_controllers::GlobalMotorState;
+
+    use super::*;
+    use crate::devices::motor_controllers::test_motor::TestMotor;
+
+    #[test]
+    fn test_motor_group() {
+        let state = Arc::new(GlobalMotorState::new());
+        let test_state = Arc::new(GlobalMotorState::new());
+        let motor = Box::new(TestMotor::new(test_state));
+        let mut motor = MotorGroup::new(vec![motor], state.clone());
+        assert_eq!(0.0, motor.get_motor_state().get_speed());
+
+        motor.set_speed(1.0);
+        assert_eq!(1.0, motor.get_motor_state().get_speed());
+
+        motor.set_speed(-1.0);
+        assert_eq!(-1.0, motor.get_motor_state().get_speed());
+
+        motor.stop();
+        assert_eq!(0.0, motor.get_motor_state().get_speed());
+
+        motor.set_speed(1.0);
+        assert_eq!(1.0, motor.get_motor_state().get_speed());
+    }
+}
