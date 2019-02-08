d185accc424248c2a29b928cf698974326a2db7e
noah <nomaxx117@gmail.com>
2019-02-08 11:49:00 -0600

    Added a test for driving.

--- src/mechatronics/drive_train/mod.rs
+++ src/mechatronics/drive_train/mod.rs
@@ -27,7 +27,9 @@ impl DriveTrain {
 
     /// Runs a cycle of the drive train, instructing all motors to do what they did last time.
     pub fn run_cycle(&mut self) {
-        if self.state.get_enabled() && self.robot_status.is_alive() {} else {
+        if self.state.get_enabled() && self.robot_status.is_alive() {
+            // TODO
+        } else {
             self.brake();
         }
     }
@@ -74,6 +76,53 @@ mod tests {
         let right = Box::new(TestMotor::new(state.get_right()));
         let _drive_train = DriveTrain::new(state.clone(), left, right, life.clone());
 
+        assert_eq!(false, state.get_enabled());
+        assert_eq!(false, state.get_current_state().get_enabled());
+        assert_eq!(0.0, state.get_left().get_speed());
+        assert_eq!(0.0, state.get_current_state().get_left().get_speed());
+        assert_eq!(0.0, state.get_right().get_speed());
+        assert_eq!(0.0, state.get_current_state().get_right().get_speed());
+    }
+
+    #[test]
+    fn test_drive() {
+        let state = Arc::new(GlobalDriveTrainState::new());
+        let life = Arc::new(GlobalLifeState::new());
+        let left = Box::new(TestMotor::new(state.get_left()));
+        let right = Box::new(TestMotor::new(state.get_right()));
+        let mut drive_train = DriveTrain::new(state.clone(), left, right, life.clone());
+
+        drive_train.enable();
+        assert_eq!(true, state.get_enabled());
+        assert_eq!(true, state.get_current_state().get_enabled());
+
+        drive_train.drive(1.0, -1.0);
+        assert_eq!(1.0, state.get_left().get_speed());
+        assert_eq!(1.0, state.get_current_state().get_left().get_speed());
+        assert_eq!(-1.0, state.get_right().get_speed());
+        assert_eq!(-1.0, state.get_current_state().get_right().get_speed());
+
+        drive_train.disable();
+        assert_eq!(false, state.get_enabled());
+        assert_eq!(false, state.get_current_state().get_enabled());
+        assert_eq!(0.0, state.get_left().get_speed());
+        assert_eq!(0.0, state.get_current_state().get_left().get_speed());
+        assert_eq!(0.0, state.get_right().get_speed());
+        assert_eq!(0.0, state.get_current_state().get_right().get_speed());
+
+        drive_train.enable();
+        drive_train.drive(1.0, -1.0);
+        life.kill();
+        drive_train.run_cycle();
+        assert_eq!(0.0, state.get_left().get_speed());
+        assert_eq!(0.0, state.get_current_state().get_left().get_speed());
+        assert_eq!(0.0, state.get_right().get_speed());
+        assert_eq!(0.0, state.get_current_state().get_right().get_speed());
+
+        life.revive();
+        drive_train.drive(1.0, -1.0);
+        state.set_enabled(false);
+        drive_train.run_cycle();
         assert_eq!(false, state.get_enabled());
         assert_eq!(false, state.get_current_state().get_enabled());
         assert_eq!(0.0, state.get_left().get_speed());
