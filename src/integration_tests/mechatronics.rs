95aabaf46d996c27dfe3c1f22722df9f44244b74
noah <nomaxx117@gmail.com>
2019-02-08 23:24:30 -0600

    Added timeout to drive test.

--- src/integration_tests/mechatronics.rs
+++ src/integration_tests/mechatronics.rs
@@ -10,8 +10,11 @@ fn test_drive() {
     let client = robot.launch_tester();
 
     client.post("/robot/modes/drive").dispatch();
+    sleep(Duration::from_millis(TIMEOUT));
 
     let response = client.post("/robot/drive_train/drive/1/-1").dispatch();
+    sleep(Duration::from_millis(TIMEOUT));
+
     assert_eq!(Status::Ok, response.status());
     assert_eq!(1.0, state.get_drive().get_left().get_current_state().get_speed());
     assert_eq!(-1.0, state.get_drive().get_right().get_current_state().get_speed());
