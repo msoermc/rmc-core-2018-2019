77142980b5ec94febf2acd356cf95c3e6497d580
noah <nomaxx117@gmail.com>
2019-02-09 11:03:07 -0600

    Added tests for stopping digging

--- src/integration_tests/mechatronics.rs
+++ src/integration_tests/mechatronics.rs
@@ -37,3 +37,24 @@ fn dig() {
     assert_eq!(Status::Ok, response.status());
     assert_eq!(DIGGING_RATE, state.get_current_state().get_intake().get_ladder().get_motor().get_speed());
 }
+
+#[test]
+fn stop_digger() {
+    let mut builder = RobotBuilder::new();
+    let state = builder.get_state();
+    builder.with_test();
+    let robot = builder.build();
+    let client = robot.launch_tester();
+
+    client.post("/robot/modes/dig").dispatch();
+    sleep(Duration::from_millis(TIMEOUT));
+
+    let _response = client.post("/robot/intake/digger/dig").dispatch();
+    sleep(Duration::from_millis(TIMEOUT));
+
+    let response = client.post("/robot/intake/digger/stop").dispatch();
+    sleep(Duration::from_millis(TIMEOUT));
+
+    assert_eq!(Status::Ok, response.status());
+    assert_eq!(0.0, state.get_current_state().get_intake().get_ladder().get_motor().get_speed());
+}
