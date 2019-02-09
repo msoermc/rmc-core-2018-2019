4162c476b1c0e9d3e993cf6836be19b31def5b16
noah <nomaxx117@gmail.com>
2019-02-08 22:38:30 -0600

    Added integration tests for ensuring motors are stopped at initialization.

--- src/integration_tests/initialization.rs
+++ src/integration_tests/initialization.rs
@@ -23,3 +23,21 @@ fn enabling() {
     assert_eq!(false, state.get_dumper().get_current_state().get_enabled());
     assert_eq!(false, state.get_intake().get_current_state().get_enabled());
 }
+
+#[test]
+fn motors() {
+    let mut builder = RobotBuilder::new();
+    let state = builder.get_state();
+    builder.with_test();
+    let robot = builder.build();
+    let _client = robot.launch_tester();
+
+    assert_eq!(0.0, state.get_intake().get_current_state().get_left_actuator().get_motor().get_speed());
+    assert_eq!(0.0, state.get_intake().get_current_state().get_right_actuator().get_motor().get_speed());
+    assert_eq!(0.0, state.get_intake().get_ladder().get_current_state().get_motor().get_speed());
+
+    assert_eq!(0.0, state.get_drive().get_current_state().get_left().get_speed());
+    assert_eq!(0.0, state.get_drive().get_current_state().get_right().get_speed());
+
+    assert_eq!(0.0, state.get_dumper().get_motor().get_current_state().get_speed());
+}
