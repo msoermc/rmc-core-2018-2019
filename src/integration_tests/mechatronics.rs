d603c4a4b632a1efa892e3231208d42442d81283
noah <nomaxx117@gmail.com>
2019-02-09 11:13:26 -0600

    Added tests for stopping the actuators

--- src/integration_tests/mechatronics.rs
+++ src/integration_tests/mechatronics.rs
@@ -118,3 +118,35 @@ fn lower() {
     assert_eq!(-MH_ACTUATOR_RATE, state.get_current_state().get_intake().get_right_actuator().get_motor().get_speed());
     assert_eq!(-MH_ACTUATOR_RATE, state.get_current_state().get_intake().get_left_actuator().get_motor().get_speed());
 }
+
+#[test]
+fn stop_actuators() {
+    let mut builder = RobotBuilder::new();
+    let state = builder.get_state();
+    builder.with_test();
+    let robot = builder.build();
+    let client = robot.launch_tester();
+
+    client.post("/robot/modes/dig").dispatch();
+    sleep(Duration::from_millis(TIMEOUT));
+
+    let response = client.post("/robot/intake/rails/lower").dispatch();
+    sleep(Duration::from_millis(TIMEOUT));
+
+    let response = client.post("/robot/intake/rails/stop").dispatch();
+    sleep(Duration::from_millis(TIMEOUT));
+
+    assert_eq!(Status::Ok, response.status());
+    assert_eq!(0.0, state.get_current_state().get_intake().get_right_actuator().get_motor().get_speed());
+    assert_eq!(0.0, state.get_current_state().get_intake().get_left_actuator().get_motor().get_speed());
+
+    let response = client.post("/robot/intake/rails/raise").dispatch();
+    sleep(Duration::from_millis(TIMEOUT));
+
+    let response = client.post("/robot/intake/rails/stop").dispatch();
+    sleep(Duration::from_millis(TIMEOUT));
+
+    assert_eq!(Status::Ok, response.status());
+    assert_eq!(0.0, state.get_current_state().get_intake().get_right_actuator().get_motor().get_speed());
+    assert_eq!(0.0, state.get_current_state().get_intake().get_left_actuator().get_motor().get_speed());
+}
