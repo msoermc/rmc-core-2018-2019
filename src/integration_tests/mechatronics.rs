42851c3766a4b20cfcedb6363d5d80c731c48506
noah <nomaxx117@gmail.com>
2019-02-09 10:58:12 -0600

    Added tests for digging

--- src/integration_tests/mechatronics.rs
+++ src/integration_tests/mechatronics.rs
@@ -16,6 +16,24 @@ fn test_drive() {
     sleep(Duration::from_millis(TIMEOUT));
 
     assert_eq!(Status::Ok, response.status());
-    assert_eq!(1.0, state.get_drive().get_left().get_current_state().get_speed());
-    assert_eq!(-1.0, state.get_drive().get_right().get_current_state().get_speed());
+    assert_eq!(1.0, state.get_current_state().get_drive().get_left().get_speed());
+    assert_eq!(-1.0, state.get_current_state().get_drive().get_right().get_speed());
+}
+
+#[test]
+fn dig() {
+    let mut builder = RobotBuilder::new();
+    let state = builder.get_state();
+    builder.with_test();
+    let robot = builder.build();
+    let client = robot.launch_tester();
+
+    client.post("/robot/modes/dig").dispatch();
+    sleep(Duration::from_millis(TIMEOUT));
+
+    let response = client.post("/robot/intake/digger/dig").dispatch();
+    sleep(Duration::from_millis(TIMEOUT));
+
+    assert_eq!(Status::Ok, response.status());
+    assert_eq!(DIGGING_RATE, state.get_current_state().get_intake().get_ladder().get_motor().get_speed());
 }
