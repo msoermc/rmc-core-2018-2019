37743f1d10adee1487e4bfaeb6eb2918b5893b80
noah <nomaxx117@gmail.com>
2019-02-08 23:36:57 -0600

    Added comprehensive tests to switching

--- src/integration_tests/switching.rs
+++ src/integration_tests/switching.rs
@@ -51,3 +51,119 @@ fn dig() {
     assert_eq!(false, state.get_dumper().get_current_state().get_enabled());
     assert_eq!(true, state.get_intake().get_current_state().get_enabled());
 }
+
+#[test]
+fn switch_dig_to_drive() {
+    let mut builder = RobotBuilder::new();
+    let state = builder.get_state();
+    builder.with_test();
+    let robot = builder.build();
+    let client = robot.launch_tester();
+
+    let _response = client.post("/robot/modes/dig").dispatch();
+
+    let response = client.post("/robot/modes/drive").dispatch();
+    assert_eq!(Status::Ok, response.status());
+
+    sleep(Duration::from_millis(TIMEOUT));
+
+    assert_eq!(true, state.get_drive().get_current_state().get_enabled());
+    assert_eq!(false, state.get_dumper().get_current_state().get_enabled());
+    assert_eq!(false, state.get_intake().get_current_state().get_enabled());
+}
+
+#[test]
+fn switch_dig_to_dump() {
+    let mut builder = RobotBuilder::new();
+    let state = builder.get_state();
+    builder.with_test();
+    let robot = builder.build();
+    let client = robot.launch_tester();
+
+    let _response = client.post("/robot/modes/dig").dispatch();
+
+    let response = client.post("/robot/modes/dump").dispatch();
+    assert_eq!(Status::Ok, response.status());
+    sleep(Duration::from_millis(TIMEOUT));
+
+    assert_eq!(false, state.get_drive().get_current_state().get_enabled());
+    assert_eq!(true, state.get_dumper().get_current_state().get_enabled());
+    assert_eq!(false, state.get_intake().get_current_state().get_enabled());
+}
+
+#[test]
+fn switch_drive_to_dump() {
+    let mut builder = RobotBuilder::new();
+    let state = builder.get_state();
+    builder.with_test();
+    let robot = builder.build();
+    let client = robot.launch_tester();
+
+    let _response = client.post("/robot/modes/drive").dispatch();
+
+    let response = client.post("/robot/modes/dump").dispatch();
+    assert_eq!(Status::Ok, response.status());
+    sleep(Duration::from_millis(TIMEOUT));
+
+    assert_eq!(false, state.get_drive().get_current_state().get_enabled());
+    assert_eq!(true, state.get_dumper().get_current_state().get_enabled());
+    assert_eq!(false, state.get_intake().get_current_state().get_enabled());
+}
+
+#[test]
+fn switch_drive_to_dig() {
+    let mut builder = RobotBuilder::new();
+    let state = builder.get_state();
+    builder.with_test();
+    let robot = builder.build();
+    let client = robot.launch_tester();
+
+    let _response = client.post("/robot/modes/drive").dispatch();
+
+    let response = client.post("/robot/modes/dig").dispatch();
+    assert_eq!(Status::Ok, response.status());
+    sleep(Duration::from_millis(TIMEOUT));
+
+    assert_eq!(false, state.get_drive().get_current_state().get_enabled());
+    assert_eq!(false, state.get_dumper().get_current_state().get_enabled());
+    assert_eq!(true, state.get_intake().get_current_state().get_enabled());
+}
+
+#[test]
+fn switch_dump_to_drive() {
+    let mut builder = RobotBuilder::new();
+    let state = builder.get_state();
+    builder.with_test();
+    let robot = builder.build();
+    let client = robot.launch_tester();
+
+    let _response = client.post("/robot/modes/dump").dispatch();
+
+    let response = client.post("/robot/modes/drive").dispatch();
+    assert_eq!(Status::Ok, response.status());
+
+    sleep(Duration::from_millis(TIMEOUT));
+
+    assert_eq!(true, state.get_drive().get_current_state().get_enabled());
+    assert_eq!(false, state.get_dumper().get_current_state().get_enabled());
+    assert_eq!(false, state.get_intake().get_current_state().get_enabled());
+}
+
+#[test]
+fn switch_dump_to_dig() {
+    let mut builder = RobotBuilder::new();
+    let state = builder.get_state();
+    builder.with_test();
+    let robot = builder.build();
+    let client = robot.launch_tester();
+
+    let _response = client.post("/robot/modes/dump").dispatch();
+
+    let response = client.post("/robot/modes/dig").dispatch();
+    assert_eq!(Status::Ok, response.status());
+    sleep(Duration::from_millis(TIMEOUT));
+
+    assert_eq!(false, state.get_drive().get_current_state().get_enabled());
+    assert_eq!(false, state.get_dumper().get_current_state().get_enabled());
+    assert_eq!(true, state.get_intake().get_current_state().get_enabled());
+}
