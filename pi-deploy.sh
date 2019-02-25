c45029bc61e1b1ea7d8762d54a93e48ac6a95869
noah <nomaxx117@gmail.com>
2019-02-25 17:03:33 -0600

    Added Rocket.toml to deployment

--- pi-deploy.sh
+++ pi-deploy.sh
@@ -10,4 +10,5 @@ cargo build --release
 scp target/debug/rmc-core rmc@${BB_IP}:~/debug
 scp target/release/rmc-core rmc@${BB_IP}:~/release
 scp enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh
+scp Rocket.toml rmc@${BB_IP}:~/Rocket.toml
 scp -r ./static/ rmc@${BB_IP}:~
