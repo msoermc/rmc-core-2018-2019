2b6d248a0b409c17a2223d46f856387efe5e530f
noah <nomaxx117@gmail.com>
2019-02-25 15:52:34 -0600

    Updated pi-deploy

--- pi-deploy.sh
+++ pi-deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-BB_IP="192.168.1.29"
-BB_PASSWD="msoe"
+BB_IP=192.168.1.29
+BB_PASSWD=msoe
 
 cargo build
 cargo build --release
+
 
 sudo sshpass -p ${BB_PASSWD} scp target/debug/rmc-core rmc@${BB_IP}:~/debug
 sudo sshpass -p ${BB_PASSWD} scp target/release/rmc-core rmc@${BB_IP}:~/release
