2373047e1f76e7cadfc002de16e1f063947c74c0
noah <nomaxx117@gmail.com>
2019-02-25 10:08:11 -0600

    Starting over with cross compilation

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,6 @@ CROSS_COMPILER_TOOLCHAIN="armv7-unknown-linux-musleabihf" # armv7 with hardware 
 REMOTE_USER="rmc"
 REMOTE_PASSWORD="msoe"
 
-# Setup pkg-config
-SYSROOT=/build/root
-
-export PKG_CONFIG_DIR=
-export PKG_CONFIG_LIBDIR=${SYSROOT}/usr/lib/pkgconfig:${SYSROOT}/usr/share/pkgconfig
-export PKG_CONFIG_SYSROOT_DIR=${SYSROOT}
-export PKG_CONFIG_ALLOW_CROSS=1
-
 # Cross compile the code
 cross build --release --target=${CROSS_COMPILER_TOOLCHAIN}
 
