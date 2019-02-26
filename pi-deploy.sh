ec151366007240cc247553daa063e5cb22bd04b4
noah <nomaxx117@gmail.com>
2019-02-26 11:33:15 -0600

    Trying rsync

--- pi-deploy.sh
+++ pi-deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 BB_IP=192.168.1.29
-BB_PASSWD='msoe'
+BB_PASSWD=msoe
 
 cargo build
 cargo build --release
 
-sshpass -p ${BB_PASSWD} scp target/debug/rmc-core rmc@${BB_IP}:~/debug
-sshpass -p ${BB_PASSWD} scp target/release/rmc-core rmc@${BB_IP}:~/release
-sshpass -p ${BB_PASSWD} scp enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh
-sshpass -p ${BB_PASSWD} scp Rocket.toml rmc@${BB_IP}:~/Rocket.toml
-sshpass -p ${BB_PASSWD} scp -r ./static/ rmc@${BB_IP}:~
+rsync -P --rsh="sshpass -p ${BB_PASSWD} ssh -l rmc" target/debug/rmc-core rmc@${BB_IP}:~/debug
+rsync -P --rsh="sshpass -p ${BB_PASSWD} ssh -l rmc" target/release/rmc-core rmc@${BB_IP}:~/release
+rsync -P --rsh="sshpass -p ${BB_PASSWD} ssh -l rmc" enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh
+rsync -P --rsh="sshpass -p ${BB_PASSWD} ssh -l rmc" Rocket.toml rmc@${BB_IP}:~/Rocket.toml
+rsync -P --rsh="sshpass -p ${BB_PASSWD} ssh -l rmc" -r ./static/ rmc@${BB_IP}:~
