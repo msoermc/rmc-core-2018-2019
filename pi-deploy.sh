d54d910df28aa7248305a599eaaac9e7977c9abe
noah <nomaxx117@gmail.com>
2019-02-26 11:18:23 -0600

    Undid last change

--- pi-deploy.sh
+++ pi-deploy.sh
@@ -7,8 +7,8 @@ cargo build
 cargo build --release
 
 
-sudo sshpass -p ${BB_PASSWD} scp target/debug/rmc-core rmc@${BB_IP}:~/debug -o StrictHostKeyChecking=no
-sudo sshpass -p ${BB_PASSWD} scp target/release/rmc-core rmc@${BB_IP}:~/release -o StrictHostKeyChecking=no
-sudo sshpass -p ${BB_PASSWD} scp enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh -o StrictHostKeyChecking=no
-sudo sshpass -p ${BB_PASSWD} scp Rocket.toml rmc@${BB_IP}:~/Rocket.toml -o StrictHostKeyChecking=no
-sudo sshpass -p ${BB_PASSWD} scp -r ./static/ rmc@${BB_IP}:~ -o StrictHostKeyChecking=no
+sudo sshpass -p ${BB_PASSWD} scp target/debug/rmc-core rmc@${BB_IP}:~/debug
+sudo sshpass -p ${BB_PASSWD} scp target/release/rmc-core rmc@${BB_IP}:~/release
+sudo sshpass -p ${BB_PASSWD} scp enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh
+sudo sshpass -p ${BB_PASSWD} scp Rocket.toml rmc@${BB_IP}:~/Rocket.toml
+sudo sshpass -p ${BB_PASSWD} scp -r ./static/ rmc@${BB_IP}:~
