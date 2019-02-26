5f0d0783711fdbededc836729a08376202058dfd
noah <nomaxx117@gmail.com>
2019-02-26 11:21:38 -0600

    Removed sudo from sshpass

--- pi-deploy.sh
+++ pi-deploy.sh
@@ -6,9 +6,8 @@ BB_PASSWD='msoe'
 cargo build
 cargo build --release
 
-
-sudo sshpass -p ${BB_PASSWD} scp target/debug/rmc-core rmc@${BB_IP}:~/debug
-sudo sshpass -p ${BB_PASSWD} scp target/release/rmc-core rmc@${BB_IP}:~/release
-sudo sshpass -p ${BB_PASSWD} scp enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh
-sudo sshpass -p ${BB_PASSWD} scp Rocket.toml rmc@${BB_IP}:~/Rocket.toml
-sudo sshpass -p ${BB_PASSWD} scp -r ./static/ rmc@${BB_IP}:~
+sshpass -p ${BB_PASSWD} scp target/debug/rmc-core rmc@${BB_IP}:~/debug
+sshpass -p ${BB_PASSWD} scp target/release/rmc-core rmc@${BB_IP}:~/release
+sshpass -p ${BB_PASSWD} scp enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh
+sshpass -p ${BB_PASSWD} scp Rocket.toml rmc@${BB_IP}:~/Rocket.toml
+sshpass -p ${BB_PASSWD} scp -r ./static/ rmc@${BB_IP}:~
