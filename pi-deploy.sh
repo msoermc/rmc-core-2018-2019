82a6a73e8a19a9c0a3454d451b8eca03f4abdf1b
noah <nomaxx117@gmail.com>
2019-02-26 11:10:48 -0600

    Added sshpass back

--- pi-deploy.sh
+++ pi-deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 BB_IP=192.168.1.29
-BB_PASSWD=msoe
+BB_PASSWD='msoe'
 
 cargo build
 cargo build --release
 
 
-scp target/debug/rmc-core rmc@${BB_IP}:~/debug
-scp target/release/rmc-core rmc@${BB_IP}:~/release
-scp enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh
-scp Rocket.toml rmc@${BB_IP}:~/Rocket.toml
-scp -r ./static/ rmc@${BB_IP}:~
+sudo sshpass -p ${BB_PASSWD} scp target/debug/rmc-core rmc@${BB_IP}:~/debug
+sudo sshpass -p ${BB_PASSWD} scp target/release/rmc-core rmc@${BB_IP}:~/release
+sudo sshpass -p ${BB_PASSWD} scp enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh
+sudo sshpass -p ${BB_PASSWD} scp Rocket.toml rmc@${BB_IP}:~/Rocket.toml
+sudo sshpass -p ${BB_PASSWD} scp -r ./static/ rmc@${BB_IP}:~
