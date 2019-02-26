0b013420ab28aa095908ed42f1fbfb37b3256f22
noah <nomaxx117@gmail.com>
2019-02-26 11:42:06 -0600

    No strict host key checks

--- pi-deploy.sh
+++ pi-deploy.sh
@@ -6,9 +6,9 @@ BB_PASSWD=msoe
 cargo build
 cargo build --release
 
-rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc" target/debug/rmc-core rmc@${BB_IP}:~/debug
-rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc" target/release/rmc-core rmc@${BB_IP}:~/release
-rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc" enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh
-rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc" Rocket.toml rmc@${BB_IP}:~/Rocket.toml
-rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc" -r ./static/ rmc@${BB_IP}:~
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" target/debug/rmc-core rmc@${BB_IP}:~/debug
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" target/release/rmc-core rmc@${BB_IP}:~/release
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" Rocket.toml rmc@${BB_IP}:~/Rocket.toml
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" -r ./static/ rmc@${BB_IP}:~
 #rsync -P --rsh="sshpass -p $PASSWORD ssh -l me8" host.ca:/cmshome/me/file /home/me/Desktop
