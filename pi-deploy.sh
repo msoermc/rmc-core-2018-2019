5537bb4d4b13041a5bd54e03e668c696b73495c7
noah <nomaxx117@gmail.com>
2019-03-03 15:35:45 -0600

    Attempting to get test builds working

--- pi-deploy.sh
+++ pi-deploy.sh
@@ -3,12 +3,16 @@
 BB_IP=192.168.1.29
 BB_PASSWD=msoe
 
+rm -rf ./test
+
 cargo build
 cargo build --release
+cargo build --tests --release -Z unstable-options --out-dir ./test
 
 rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" target/debug/rmc-core rmc@${BB_IP}:~/debug
 rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" target/release/rmc-core rmc@${BB_IP}:~/release
 rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh
 rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" Rocket.toml rmc@${BB_IP}:~/Rocket.toml
 rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" -r ./static/ rmc@${BB_IP}:~/static/
-#rsync -P --rsh="sshpass -p $PASSWORD ssh -l me8" host.ca:/cmshome/me/file /home/me/Desktop
+
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" -r ./test/* rmc@${BB_IP}:~/test/
