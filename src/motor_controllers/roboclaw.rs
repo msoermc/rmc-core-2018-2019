b2a7bd854b8129872001ce3ed8d9cc257b26175a
noah <nomaxx117@gmail.com>
2019-03-02 18:46:16 -0600

    Fixed roboclaw

--- src/motor_controllers/roboclaw.rs
+++ src/motor_controllers/roboclaw.rs
@@ -1,11 +1,9 @@
-use std::sync::Arc;
-
 use crate::motor_controllers::GlobalMotorState;
-use crate::pinouts::analog::output::AnalogOutput;
 use crate::pinouts::analog::output::PwmOutput;
-use crate::pinouts::digital::output::DigitalOutput;
 
 use super::MotorController;
+
+const OUTPUT_VOLTAGE: f32 = 3.3;
 
 pub struct RoboClaw {
     pwm: Box<PwmOutput>,
@@ -14,11 +12,12 @@ pub struct RoboClaw {
 
 impl MotorController for RoboClaw {
     fn set_speed(&mut self, new_speed: f32) {
-        unimplemented!()
+        let value = (new_speed + 1.0) / OUTPUT_VOLTAGE;
+        self.pwm.set_value(value);
     }
 
     fn stop(&mut self) {
-        self.pwm.set_value(0.0);
+        self.set_speed(0.0);
         self.state.set_speed(0.0);
     }
 
@@ -29,10 +28,13 @@ impl MotorController for RoboClaw {
 
 impl RoboClaw {
     pub fn new(pwm: Box<PwmOutput>) -> Self {
-        RoboClaw {
+        let mut result = RoboClaw {
             pwm,
             state: GlobalMotorState::new(),
-        }
+        };
+
+        result.set_speed(0.0);
+        result
     }
 }
 
@@ -40,5 +42,69 @@ impl RoboClaw {
 impl Drop for RoboClaw {
     fn drop(&mut self) {
         self.stop();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use atomic::Atomic;
+    use atomic::Ordering;
+
+    use crate::pinouts::analog::output::TestPwm;
+
+    use super::*;
+
+    fn output_to_voltage(val: f32) -> f32 {
+        (val + 1.0) / OUTPUT_VOLTAGE
+    }
+
+    #[test]
+    fn test_init() {
+        let output = Arc::new(atomic::Atomic::new(0.0));
+        let pwm = Box::new(TestPwm::new(output.clone()));
+
+        let mut motor = RoboClaw::new(pwm);
+
+        assert_eq!(output_to_voltage(0.0), output.load(Ordering::SeqCst));
+    }
+
+    #[test]
+    fn forwards() {
+        let output = Arc::new(Atomic::new(0.0));
+        let pwm = Box::new(TestPwm::new(output.clone()));
+
+        let mut motor = RoboClaw::new(pwm);
+
+        motor.set_speed(1.0);
+
+        assert_eq!(2.0 / OUTPUT_VOLTAGE, output.load(Ordering::SeqCst));
+    }
+
+    #[test]
+    fn backwards() {
+        let output = Arc::new(Atomic::new(0.0));
+        let pwm = Box::new(TestPwm::new(output.clone()));
+
+        let mut motor = RoboClaw::new(pwm);
+
+        motor.set_speed(-1.0);
+
+        assert_eq!(0.0, output.load(Ordering::SeqCst));
+    }
+
+    #[test]
+    fn brake() {
+        let output = Arc::new(Atomic::new(0.0));
+        let pwm = Box::new(TestPwm::new(output.clone()));
+
+        let mut motor = RoboClaw::new(pwm);
+
+        motor.set_speed(1.0);
+
+        motor.stop();
+
+        assert_eq!(1.0 / OUTPUT_VOLTAGE, output.load(Ordering::SeqCst));
     }
 }
