7b1e8c9f8e89538b9dbc172f07d8f5a5ea6efc9e
noah <nomaxx117@gmail.com>
2019-03-05 12:18:05 -0600

    Moving compiled build files to special directory to clean up the home directory.

--- pi-deploy.sh
+++ pi-deploy.sh
@@ -2,6 +2,7 @@
 
 BB_IP=192.168.1.29
 BB_PASSWD=msoe
+ROBOT_DIR=~/robot
 
 rm -rf ./test-release
 rm -rf ./test-debug
@@ -11,11 +12,11 @@ cargo build --release
 cargo build --tests --release -Z unstable-options --out-dir ./test-release
 cargo build --tests -Z unstable-options --out-dir ./test-debug
 
-rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" target/debug/rmc-core rmc@${BB_IP}:~/debug
-rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" target/release/rmc-core rmc@${BB_IP}:~/release
-rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" enable-pwm.sh rmc@${BB_IP}:~/enable-pwm.sh
-rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" Rocket.toml rmc@${BB_IP}:~/Rocket.toml
-rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" -r ./static/ rmc@${BB_IP}:~/static/
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" target/debug/rmc-core rmc@${BB_IP}:${ROBOT_DIR}/debug
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" target/release/rmc-core rmc@${BB_IP}:${ROBOT_DIR}/release
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" enable-pwm.sh rmc@${BB_IP}:${ROBOT_DIR}/enable-pwm.sh
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" Rocket.toml rmc@${BB_IP}:${ROBOT_DIR}/Rocket.toml
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" -r ./static/ rmc@${BB_IP}:${ROBOT_DIR}/static/
 
-rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" -r ./test-release/ rmc@${BB_IP}:~/test-release/
-rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" -r ./test-debug/ rmc@${BB_IP}:~/test-debug/
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" -r ./test-release/ rmc@${BB_IP}:${ROBOT_DIR}/test-release/
+rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" -r ./test-debug/ rmc@${BB_IP}:${ROBOT_DIR}/test-debug/
