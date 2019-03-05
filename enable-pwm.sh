d0c50f1bc93c25370ac6a23a4e8941d4bbf8e56a
noah <nomaxx117@gmail.com>
2019-03-05 16:48:36 -0600

    Commented out slots

--- enable-pwm.sh
+++ enable-pwm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 PASSWORD="msoe"
-echo ${PASSWORD} | sudo -S bash -c "echo am33xx_pwm > /sys/devices/platform/bone_capemgr/slots"
-echo ${PASSWORD} | sudo -S bash -c "echo cape-universal > /sys/devices/platform/bone_capemgr/slots"
+#echo ${PASSWORD} | sudo -S bash -c "echo am33xx_pwm > /sys/devices/platform/bone_capemgr/slots"
+#echo ${PASSWORD} | sudo -S bash -c "echo cape-universal > /sys/devices/platform/bone_capemgr/slots"
 
 config-pin P8_13 pwm
 config-pin P9_14 pwm
