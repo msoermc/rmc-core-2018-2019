17a472710a9d1b4f2014db7b3d8258135e4c1152
noah <nomaxx117@gmail.com>
2019-03-07 18:18:28 -0600

    Added back sensors and added monitor for digital inputs.

---
+++ src/sensors/digital.rs
@@ -0,0 +1,39 @@
+use std::sync::Arc;
+use std::sync::atomic::AtomicBool;
+
+use atomic::Ordering;
+
+use crate::framework::Runnable;
+use crate::pinouts::digital::input::DigitalInput;
+
+pub struct DigitalInputMonitor {
+    input: Box<DigitalInput>,
+    update_field: Arc<AtomicBool>,
+    default: bool,
+}
+
+impl Runnable for DigitalInputMonitor {
+    fn init(&mut self) {
+        if let Some(val) = self.input.get_value() {
+            self.update_field.store(val, Ordering::Relaxed)
+        } else {
+            self.update_field.store(self.default, Ordering::Relaxed)
+        }
+    }
+
+    fn run(&mut self) {
+        if let Some(val) = self.input.get_value() {
+            self.update_field.store(val, Ordering::Relaxed)
+        }
+    }
+}
+
+impl DigitalInputMonitor {
+    pub fn new(input: Box<DigitalInput>, update_field: Arc<AtomicBool>, default: bool) -> Self {
+        Self {
+            input,
+            update_field,
+            default,
+        }
+    }
+}
