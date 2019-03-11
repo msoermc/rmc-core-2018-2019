999764b84bae852c6e7556bbbeda838bb4d325bd
noah <nomaxx117@gmail.com>
2019-03-11 12:43:37 -0500

    Cleaning up dumper tests

--- src/mechatronics/dumper/tests.rs
+++ src/mechatronics/dumper/tests.rs
@@ -2,76 +2,33 @@ use super::*;
 use std::sync::Arc;
 use crate::motor_controllers::test_motor::TestMotor;
 
-#[test]
-fn test_setup() {
+fn setup() -> (Arc<GlobalLifeState>, Arc<GlobalDumperState>, Dumper) {
     let life = Arc::new(GlobalLifeState::new());
     let state = Arc::new(GlobalDumperState::new());
     let motor = Box::new(TestMotor::new(state.get_motor()));
 
-    let _dumper = Dumper::new(life.clone(), motor, state.clone());
+    let dumper = Dumper::new(life.clone(), motor, state.clone());
+
+    (life, state, dumper)
+}
+
+#[test]
+fn initial_state() {
+    let (_, state, _) = setup();
 
     assert_eq!(0.0, state.get_motor().get_speed());
     assert_eq!(false, state.get_enabled());
 }
 
 #[test]
-fn test_dump() {
-    let life = Arc::new(GlobalLifeState::new());
-    let state = Arc::new(GlobalDumperState::new());
-    let motor = Box::new(TestMotor::new(state.get_motor()));
+fn initial_immobility() {
+    let (_, state, mut dumper) = setup();
 
-    let mut dumper = Dumper::new(life.clone(), motor, state.clone());
-    dumper.enable();
-
-    dumper.dump();
-    assert_eq!(DUMPING_RATE, state.get_motor().get_speed());
-    dumper.run_cycle();
-    assert_eq!(DUMPING_RATE, state.get_motor().get_speed());
-
-    dumper.dump();
-    assert_eq!(DUMPING_RATE, state.get_motor().get_speed());
-
-    dumper.disable();
-    assert_eq!(0.0, state.get_motor().get_speed());
     dumper.dump();
     assert_eq!(0.0, state.get_motor().get_speed());
-
-    dumper.enable();
-    dumper.dump();
-    assert_eq!(DUMPING_RATE, state.get_motor().get_speed());
-}
-
-#[test]
-fn test_reset() {
-    let life = Arc::new(GlobalLifeState::new());
-    let state = Arc::new(GlobalDumperState::new());
-    let motor = Box::new(TestMotor::new(state.get_motor()));
-
-    let mut dumper = Dumper::new(life.clone(), motor, state.clone());
-    dumper.enable();
-
-    dumper.reset();
-    assert_eq!(DUMPER_RESET_RATE, state.get_motor().get_speed());
-    assert_eq!(DUMPER_RESET_RATE, state.get_current_state().get_motor().get_speed());
-    dumper.run_cycle();
-    assert_eq!(DUMPER_RESET_RATE, state.get_motor().get_speed());
-    assert_eq!(DUMPER_RESET_RATE, state.get_current_state().get_motor().get_speed());
-
-    life.revive();
-    dumper.reset();
-    assert_eq!(DUMPER_RESET_RATE, state.get_motor().get_speed());
-    assert_eq!(DUMPER_RESET_RATE, state.get_current_state().get_motor().get_speed());
-
-    dumper.disable();
     assert_eq!(false, state.get_enabled());
-    assert_eq!(false, state.get_current_state().get_enabled());
-    assert_eq!(0.0, state.get_motor().get_speed());
-    assert_eq!(0.0, state.get_current_state().get_motor().get_speed());
 
-    dumper.enable();
-    assert_eq!(true, state.get_enabled());
-    assert_eq!(true, state.get_current_state().get_enabled());
     dumper.reset();
-    assert_eq!(DUMPER_RESET_RATE, state.get_motor().get_speed());
-    assert_eq!(DUMPER_RESET_RATE, state.get_current_state().get_motor().get_speed());
+    assert_eq!(0.0, state.get_motor().get_speed());
+    assert_eq!(false, state.get_enabled());
 }
