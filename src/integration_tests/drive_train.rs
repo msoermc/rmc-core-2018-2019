46bd99edf33ae3f8e7a183423b90ae14b0c7d54d
noah <nomaxx117@gmail.com>
2019-03-11 11:45:28 -0500

    Added test for braking

--- src/integration_tests/drive_train.rs
+++ src/integration_tests/drive_train.rs
@@ -47,3 +47,16 @@ fn change_direction() {
     assert_eq!(-1.0, state.get_drive().get_left().get_speed());
     assert_eq!(-1.0, state.get_drive().get_right().get_speed());
 }
+
+#[test]
+fn brake() {
+    let (state, client) = setup();
+    client.post(get_drive_url(1.0, 1.0)).dispatch();
+    sleep(Duration::from_millis(TIMEOUT_MILLIS));
+
+    client.post(get_brake_url()).dispatch();
+    sleep(Duration::from_millis(TIMEOUT_MILLIS));
+
+    assert_eq!(0.0, state.get_drive().get_left().get_speed());
+    assert_eq!(0.0, state.get_drive().get_right().get_speed());
+}
