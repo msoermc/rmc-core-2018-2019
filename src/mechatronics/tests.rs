6b0c6c16ac88d1181a88ee5e3c8e1de2180d1458
noah <nomaxx117@gmail.com>
2019-03-11 14:30:10 -0500

    Added tests for killing intake and state

---
+++ src/mechatronics/tests.rs
@@ -0,0 +1,86 @@
+use std::sync::Arc;
+use std::sync::mpsc::sync_channel;
+
+use crate::mechatronics::bucket_ladder::Intake;
+use crate::mechatronics::commands::RobotCommandFactory;
+use crate::mechatronics::controller::RobotController;
+use crate::mechatronics::drive_train::DriveTrain;
+use crate::mechatronics::dumper::Dumper;
+use crate::motor_controllers::test_motor::TestMotor;
+use crate::status::life::GlobalLifeState;
+use crate::status::robot_state::GlobalRobotState;
+
+use super::*;
+use crate::framework::Runnable;
+
+fn setup() -> (Arc<GlobalRobotState>, RobotController, RobotCommandFactory) {
+    let state = Arc::new(GlobalRobotState::new());
+
+    let digger = Box::new(TestMotor::new(state.get_intake().get_digger()));
+    let intake_height = Box::new(TestMotor::new(state.get_intake().get_actuator()));
+    let intake = Intake::new(digger, intake_height, state.get_intake(), state.get_life());
+
+    let dumper_motor = Box::new(TestMotor::new(state.get_dumper().get_motor()));
+    let dumper = Dumper::new(state.get_life(), dumper_motor, state.get_dumper());
+
+    let left_drive = Box::new(TestMotor::new(state.get_drive().get_left()));
+    let right_drive = Box::new(TestMotor::new(state.get_drive().get_right()));
+    let drive_train = DriveTrain::new(state.get_drive(), left_drive, right_drive, state.get_life());
+
+    let (sender, receiver) = sync_channel(10);
+
+//    let messenger = RobotMessenger::new(sender);
+    let controller = RobotController::new(receiver, drive_train, dumper, intake, state.get_life(), state.get_cycle_counter());
+
+    (state, controller, RobotCommandFactory::new())
+}
+
+#[test]
+fn kill_status() {
+    let (state, mut controller, factory) = setup();
+    controller.handle_message(Box::new(factory.generate_kill_command()));
+
+    assert_eq!(false, state.get_life().is_alive());
+}
+
+#[test]
+fn kill_drive() {
+    let (state, mut controller, factory) = setup();
+
+    controller.get_drive_train().enable();
+    controller.get_drive_train().drive(1.0, 1.0);
+
+    controller.handle_message(Box::new(factory.generate_kill_command()));
+
+    assert_eq!(true, state.get_drive().get_enabled());
+    assert_eq!(0.0, state.get_drive().get_left().get_speed());
+    assert_eq!(0.0, state.get_drive().get_right().get_speed());
+}
+
+#[test]
+fn kill_dumper() {
+    let (state, mut controller, factory) = setup();
+
+    controller.get_dumper().enable();
+    controller.get_dumper().dump();
+
+    controller.handle_message(Box::new(factory.generate_kill_command()));
+
+    assert_eq!(true, state.get_dumper().get_enabled());
+    assert_eq!(0.0, state.get_dumper().get_motor().get_speed());
+}
+
+#[test]
+fn kill_intake() {
+    let (state, mut controller, factory) = setup();
+
+    controller.get_intake().enable();
+    controller.get_intake().dig();
+    controller.get_intake().raise();
+
+    controller.handle_message(Box::new(factory.generate_kill_command()));
+
+    assert_eq!(true, state.get_intake().get_enabled());
+    assert_eq!(0.0, state.get_intake().get_digger().get_speed());
+    assert_eq!(0.0, state.get_intake().get_actuator().get_speed());
+}
