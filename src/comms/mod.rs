a9d7853e27f35eeb92345f493398c774354d1bbb
noah <nomaxx117@gmail.com>
2019-03-12 13:51:49 -0500

    Changing REST API

--- src/comms/mod.rs
+++ src/comms/mod.rs
@@ -12,6 +12,7 @@ use crate::mechatronics::commands::RobotCommandFactory;
 use crate::mechatronics::RobotMessenger;
 use crate::status::robot_state::GlobalRobotState;
 use crate::status::robot_state::RobotStateInstance;
+use crate::main;
 
 #[cfg(test)]
 mod tests;
@@ -29,129 +30,37 @@ struct ServerState {
 
 /// Prepares the server for launch.
 pub fn stage(messenger: RobotMessenger, state: Arc<GlobalRobotState>, command_factory: RobotCommandFactory) -> Rocket {
-    let state = ServerState {
-        messenger,
-        state,
-        command_factory,
-    };
     rocket::ignite()
+        .manage(messenger)
         .manage(state)
+        .manage(command_factory)
         .mount("/",
-               routes![handle_drive,
-                              get_state,
-                              kill,
-                              revive,
-                              brake,
-                              dig,
-                              dump,
-                              lower_digger,
-                              raise_digger,
-                              reset_dumper,
-                              stop_digger,
-                              stop_dumper,
-                              stop_rails,
-                              switch_mode,
+               routes![get_state,
                               index,
                               files,
                               favicon,])
 }
 
+#[derive(Deserialize, Serialize)]
+enum RobotMode {
+    Digging,
+    Driving,
+    Dumping,
+}
+
+#[post("/robot/mode", data = "<mode>")]
+fn update_mode(mode: Json<RobotMode>, state: State<ServerState>, messenger: State<RobotMessenger>, factory: State<RobotCommandFactory>) {
+    match mode.into_inner() {
+        RobotMode::Digging => messenger.send_command(Box::new(factory.generate_intake_switch_command())),
+        RobotMode::Driving => messenger.send_command(Box::new(factory.generate_drive_switch_command())),
+        RobotMode::Dumping => messenger.send_command(Box::new(factory.generate_dumper_switch_command())),
+    }
+}
+
 /// Responds with the current state of the robot, as a JSON object.
-#[get("/robot/state")]
-fn get_state(state: State<ServerState>) -> Json<RobotStateInstance> {
-    Json(state.state.get_current_state())
-}
-
-/// Switches the current mode of the robot.
-/// The allowed modes are `dig`, `dump`, and `drive`.
-/// When we switch to a mode, only that subsystem is enabled and the others will be disabled.
-#[post("/robot/modes/<mode>")]
-fn switch_mode(mode: String, state: State<ServerState>) -> Status {
-    match mode.as_str() {
-        "dig" => state.messenger.send_command(Box::new(state.command_factory.generate_intake_switch_command())),
-        "dump" => state.messenger.send_command(Box::new(state.command_factory.generate_dumper_switch_command())),
-        "drive" => state.messenger.send_command(Box::new(state.command_factory.generate_drive_switch_command())),
-        _ => return Status::BadRequest,
-    }
-
-    Status::Ok
-}
-
-/// Runs the drive train at the provided speeds.
-#[post("/robot/drive_train/drive/<left>/<right>")]
-fn handle_drive(left: f32, right: f32, state: State<ServerState>) -> Status {
-    if let Some(command) = state.command_factory.generate_drive_command(left, right) {
-        state.messenger.send_command(Box::new(command));
-        Status::Ok
-    } else {
-        Status::BadRequest
-    }
-}
-
-/// Starts the dumper.
-#[post("/robot/dumper/dump")]
-fn dump(state: State<ServerState>) {
-    state.messenger.send_command(Box::new(state.command_factory.generate_dump_command()));
-}
-
-/// Resets the dumper
-#[post("/robot/dumper/reset")]
-fn reset_dumper(state: State<ServerState>) {
-    state.messenger.send_command(Box::new(state.command_factory.generate_reset_dumper_command()));
-}
-
-/// Stops the dumper.
-#[post("/robot/dumper/stop")]
-fn stop_dumper(state: State<ServerState>) {
-    state.messenger.send_command(Box::new(state.command_factory.generate_stop_dumper_command()));
-}
-
-/// Raises the actuators on the digger.
-#[post("/robot/intake/rails/raise")]
-fn raise_digger(state: State<ServerState>) {
-    state.messenger.send_command(Box::new(state.command_factory.generate_raise_actuators_command()));
-}
-
-/// Lower the actuators on the digger.
-#[post("/robot/intake/rails/lower")]
-fn lower_digger(state: State<ServerState>) {
-    state.messenger.send_command(Box::new(state.command_factory.generate_lower_actuators_command()));
-}
-
-/// Stops the actuators.
-#[post("/robot/intake/rails/stop")]
-fn stop_rails(state: State<ServerState>) {
-    state.messenger.send_command(Box::new(state.command_factory.generate_stop_actuators_command()));
-}
-
-/// Starts the digger.
-#[post("/robot/intake/digger/dig")]
-fn dig(state: State<ServerState>) {
-    state.messenger.send_command(Box::new(state.command_factory.generate_dig_command()));
-}
-
-/// Stops the digger.
-#[post("/robot/intake/digger/stop")]
-fn stop_digger(state: State<ServerState>) {
-    state.messenger.send_command(Box::new(state.command_factory.generate_stop_digger_command()));
-}
-
-/// Kills the robot. When this command is invoked, all physical motion on the robot ceases.
-#[post("/robot/kill")]
-fn kill(state: State<ServerState>) {
-    state.messenger.send_command(Box::new(state.command_factory.generate_kill_command()));
-}
-
-/// Causes the drive train to begin braking.
-#[post("/robot/drive_train/brake")]
-fn brake(state: State<ServerState>) {
-    state.messenger.send_command(Box::new(state.command_factory.generate_brake_command()));
-}
-
-/// Revives a dead robot, allowing further motion.
-#[post("/robot/revive")]
-fn revive(state: State<ServerState>) {
-    state.messenger.send_command(Box::new(state.command_factory.generate_revive_command()));
+#[get("/robot")]
+fn get_state(state: State<Arc<GlobalRobotState>>) -> Json<RobotStateInstance> {
+    Json(state.get_current_state())
 }
 
 #[get("/favicon.ico")]
