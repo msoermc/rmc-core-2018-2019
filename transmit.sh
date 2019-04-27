47d5b59b244f1520c923deead9b4ae65e10ba91e
noah <nomaxx117@gmail.com>
2019-04-27 12:58:24 -0500

    Split up the scripts

--- transmit.sh
+++ transmit.sh
@@ -4,14 +4,6 @@ BB_IP=192.168.1.29
 BB_PASSWD=msoe
 ROBOT_DIR=/home/rmc/robot
 
-rm -rf ./test-release
-rm -rf ./test-debug
-
-cargo build
-cargo build --release
-cargo build --tests --release -Z unstable-options --out-dir ./test-release
-cargo build --tests -Z unstable-options --out-dir ./test-debug
-
 rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" target/debug/rmc-core rmc@${BB_IP}:${ROBOT_DIR}/debug
 rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" target/release/rmc-core rmc@${BB_IP}:${ROBOT_DIR}/release
 rsync -P --rsh="sshpass -p $BB_PASSWD ssh -l rmc -o StrictHostKeyChecking=no" enable-pwm.sh rmc@${BB_IP}:${ROBOT_DIR}/enable-pwm.sh
